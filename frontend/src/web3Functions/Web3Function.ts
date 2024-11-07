be9a34bf51f1fd2f5901d9748b5705a3700fa021
HiteshMittal07 <smartyboy.hm15@gmail.com>
2024-11-07 18:38:35 +0530

    feat: completed web3 function implementation using typescript functions

--- frontend/src/web3Functions/Web3Function.ts
+++ frontend/src/web3Functions/Web3Function.ts
@@ -2,16 +2,28 @@ import {
   Web3Function,
   Web3FunctionContext,
 } from "@gelatonetwork/web3-functions-sdk";
-import { SponsoredCall } from "../RelayERC2771/SponsoredCallERC2771";
+import { Contract } from "ethers";
 
+const ABI = ["function updateNumber(uint256 _number)"];
 Web3Function.onRun(async (context: Web3FunctionContext) => {
   const { userArgs, multiChainProvider } = context;
 
   const provider = multiChainProvider.default();
+  const contractAddress =
+    (userArgs.contract as string) ??
+    "0xC645269288246e09B10B106248AB4CfbF24af1eA";
+  const contract = new Contract(contractAddress, ABI, provider as any);
+  const updatedNumber = (userArgs._number as number) ?? 100;
 
-  await SponsoredCall(150);
   return {
     canExec: true,
-    callData: [],
+    callData: [
+      {
+        to: contractAddress,
+        data: contract.interface.encodeFunctionData("updateNumber", [
+          updatedNumber,
+        ]),
+      },
+    ],
   };
 });
